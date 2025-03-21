9b8b6067910c17e04c7d9dcd51f992c581da02f2
Heron Amaral <heron.amaral@gmail.com>
2025-03-20 23:13:45 -0300

    fix: improve type safety in AuthGuard by specifying request type and payload structure

--- src/infrastructure/security/AuthGuard/auth.guard.ts
+++ src/infrastructure/security/AuthGuard/auth.guard.ts
@@ -12,15 +12,18 @@ export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request: Request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
-      });
+      const payload = await this.jwtService.verifyAsync<{ [key: string]: any }>(
+        token,
+        {
+          secret: process.env.JWT_SECRET,
+        },
+      );
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
