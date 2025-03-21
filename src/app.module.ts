108c0611a2ec96de1a8f4cb54806fb5940c54e0d
Heron Amaral <heron.amaral@gmail.com>
2025-03-21 16:37:32 -0300

    feat: implement rate limiting and security enhancements with Throttler and Helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './infrastructure/http/controllers/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { SuccessLoggerInterceptor } from './infrastructure/filters/success.interceptor';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100,
+        },
+      ],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -19,6 +29,10 @@ import { SuccessLoggerInterceptor } from './infrastructure/filters/success.inter
     {
       provide: APP_INTERCEPTOR,
       useClass: SuccessLoggerInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
