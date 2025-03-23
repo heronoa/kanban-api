08dbe1bf541b813d5931e4467ba8c8db153eb6eb
Heron Amaral <heron.amaral@gmail.com>
2025-03-23 17:52:40 -0300

    feat: enhance Task and User controllers with Swagger documentation for improved API clarity

--- src/infrastructure/http/controllers/task/task.controller.ts
+++ src/infrastructure/http/controllers/task/task.controller.ts
@@ -20,6 +20,8 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ListTaskDTO } from '@/domain/dto/task/list-task.dto';
 
 @Controller('tasks')
 @UseGuards(AuthGuard)
@@ -34,10 +36,42 @@ export class TasksController {
   ) {}
 
   @Get()
+  @ApiOperation({
+    summary:
+      'List Tasks - List all tasks (Limitations: Admin may list all tasks, User can only list tasks from a project that he is part of)',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of tasks',
+    type: ListTaskDTO,
+    example: {
+      value: {
+        totalPages: 1,
+        page: 1,
+        perPage: 10,
+        totalCount: 1,
+        tasks: [
+          {
+            id: '1',
+            title: 'Task 1',
+            description: 'Description of task 1',
+            status: 'TODO',
+            createdAt: '2021-09-01T20:00:00.000Z',
+          },
+        ],
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async listTasks(
     @Request() req: AuthRequest,
     @Query('page') page = 1,
     @Query('limit') limit = 10,
+    @Query('projectId') projectId: string,
+    @Query('status') status: string,
   ) {
     const user = req.user;
 
@@ -46,10 +80,36 @@ export class TasksController {
       userRole: user.role,
       page: Number(page),
       perPage: Number(limit),
+      query: {
+        projectId,
+        status,
+      },
     });
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary:
+      'Get Task - Get a task by id (Limitations: Admin can get any task by id but a User can only get tasks owned by a project that he is part of',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Task',
+    type: Task,
+    example: {
+      value: {
+        id: '1',
+        title: 'Task 1',
+        description: 'Description of task 1',
+        status: 'TODO',
+        createdAt: '2021-09-01T20:00:00.000Z',
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async getTaskById(@Request() req: AuthRequest, @Param('id') id: string) {
     const user = req.user;
 
@@ -61,6 +121,24 @@ export class TasksController {
   }
 
   @Post()
+  @ApiOperation({
+    summary:
+      'Create Task - Create a new task (Auth Guarded) Limitations: User can only create tasks in projects that he is part of, Admin can create tasks in any project',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Task created',
+    type: Task,
+    example: {
+      value: {
+        id: '1',
+        title: 'Task 1',
+        description: 'Description of task 1',
+        status: 'TODO',
+        createdAt: '2021-09-01T20:00:00.000Z',
+      },
+    },
+  })
   async createTask(@Request() req: AuthRequest, @Body() taskData: Task) {
     const user = req.user;
 
@@ -68,6 +146,28 @@ export class TasksController {
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary:
+      'Update Task - Update a task by id Limitations: User can only update task from projects that he is part of, Admin can update any task',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Task updated',
+    type: Task,
+    example: {
+      value: {
+        id: '1',
+        title: 'Task 1',
+        description: 'Description of task 1',
+        status: 'TODO',
+        createdAt: '2021-09-01T20:00:00.000Z',
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async updateTask(
     @Request() req: AuthRequest,
     @Body() taskData: Partial<Task>,
@@ -79,6 +179,28 @@ export class TasksController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary:
+      'Delete Task - Delete a task by id Limitations: User can only delete task that he is owner, Admin can delete any task',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Task deleted',
+    type: Task,
+    example: {
+      value: {
+        id: '1',
+        title: 'Task 1',
+        description: 'Description of task 1',
+        status: 'TODO',
+        createdAt: '2021-09-01T20:00:00.000Z',
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async deleteTask(@Request() req: AuthRequest, @Param('id') id: string) {
     const user = req.user;
 
@@ -86,6 +208,24 @@ export class TasksController {
   }
 
   @Patch(':id/move')
+  @ApiOperation({
+    summary:
+      'Move Task - Move a task to another project Limitations: User can only move tasks from projects that he is part of to a project that he is part of, Admin can move any task',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Task moved',
+    type: Task,
+    example: {
+      value: {
+        id: '1',
+        title: 'Task 1',
+        description: 'Description of task 1',
+        status: 'TODO',
+        createdAt: '2021-09-01T20:00:00.000Z',
+      },
+    },
+  })
   async moveTask(
     @Request() req: AuthRequest,
     @Param('id') taskId: string,
--- src/infrastructure/http/controllers/user/user.controller.ts
+++ src/infrastructure/http/controllers/user/user.controller.ts
@@ -18,6 +18,8 @@ import { ProfileUserUseCase } from '@/application/use-cases/user/get-profile.use
 import { UpdateUserUseCase } from '@/application/use-cases/user/update-user.use-case';
 import { DeleteUserUseCase } from '@/application/use-cases/user/delete-user.use-case';
 import { User } from '@/domain/entities/user.entity';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ListUserDTO } from '@/domain/dto/user/list-user.dto';
 
 @Controller('users')
 @UseGuards(AuthGuard)
@@ -30,6 +32,14 @@ export class UserController {
   ) {}
 
   @Get('profile')
+  @ApiOperation({
+    summary: 'Get Profile - Get the user profile (Auth Guarded)',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User profile',
+    type: User,
+  })
   async getProfile(@Request() req: AuthRequest) {
     console.log('user', req.user);
 
@@ -37,6 +47,32 @@ export class UserController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'List Users - List all users (Admin only)' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of users',
+    type: ListUserDTO,
+    example: {
+      value: {
+        users: [
+          {
+            id: '1',
+            name: 'Test User',
+            email: 'test@gmail.com',
+            role: 'USER',
+            createdAt: '2021-09-01T20:00:00.000Z',
+          },
+        ],
+        page: 1,
+        perPage: 10,
+        total: 1,
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @UseGuards(RolesGuard)
   @Roles('ADMIN')
   async listUsers(@Query('page') page = 1, @Query('limit') limit = 10) {
@@ -47,6 +83,16 @@ export class UserController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get User By Id - Get user by id (Admin Only)' })
+  @ApiResponse({
+    status: 200,
+    description: 'User',
+    type: User,
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @UseGuards(RolesGuard)
   @Roles('ADMIN')
   async getUserById(@Param('id') id: string) {
@@ -54,6 +100,19 @@ export class UserController {
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary:
+      'Update User - Update user data (Limitations: User can only update his own data, Admin can update any data',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated',
+    type: User,
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async updateUser(
     @Param('id') id: string,
     @Request() req: AuthRequest,
@@ -62,6 +121,19 @@ export class UserController {
     return this.updateUserUseCase.execute({ id, user: req.user, data });
   }
 
+  @ApiOperation({
+    summary:
+      'Delete User - Delete user (Limitations: User can only delete his own data, Admin can delete any data',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User deleted',
+    type: User,
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Delete(':id')
   async deleteUser(@Param('id') id: string, @Request() req: AuthRequest) {
     return this.deleteUserUseCase.execute({ id, user: req.user });
