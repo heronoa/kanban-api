c81d4733635c5f6a39bf3ac5035602cf66578333
Heron Amaral <heron.amaral@gmail.com>
2025-03-24 10:35:12 -0300

    test: update integration tests for project and task assignments

--- test/integration/Projects-Tasks-Assignments.test.ts
+++ test/integration/Projects-Tasks-Assignments.test.ts
@@ -7,11 +7,13 @@ import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '@/app.module';
 import { PrismaService } from '@/infrastructure/database/prisma.service';
+import { AuthResponseDto } from '@/domain/dto/auth/auth-reponse.dto';
 
 describe('Kanban API - Testes de Integração', () => {
   let app: INestApplication;
   let prisma: PrismaService;
   let token: string;
+  let token2: string;
   let projectId: string;
   let taskId: string;
   let userId1: string;
@@ -35,37 +37,38 @@ describe('Kanban API - Testes de Integração', () => {
 
   it('Deve registrar usuários', async () => {
     const res1 = await request(app.getHttpServer())
-      .post('/api/v1/auth/register')
+      .post('/auth/register')
       .send({
         email: 'user1@example.com',
         password: 'password123',
         name: 'User 1',
       });
     expect(res1.status).toBe(201);
-    userId1 = res1.body.id;
+    userId1 = (res1.body as AuthResponseDto).user.id;
 
     const res2 = await request(app.getHttpServer())
-      .post('/api/v1/auth/register')
+      .post('/auth/register')
       .send({
         email: 'user2@example.com',
         password: 'password123',
         name: 'User 2',
       });
     expect(res2.status).toBe(201);
-    userId2 = res2.body.id;
+    userId2 = (res2.body as AuthResponseDto).user.id;
+    token2 = (res2.body as AuthResponseDto).token;
   });
 
   it('Deve autenticar um usuário e retornar um token', async () => {
     const res = await request(app.getHttpServer())
-      .post('/api/v1/auth/login')
+      .post('/auth/login')
       .send({ email: 'user1@example.com', password: 'password123' });
-    expect(res.status).toBe(200);
-    token = res.body.access_token;
+    expect(res.status).toBe(201);
+    token = (res.body as AuthResponseDto).token;
   });
 
   it('Deve criar um projeto', async () => {
     const res = await request(app.getHttpServer())
-      .post('/api/v1/projects')
+      .post('/projects')
       .set('Authorization', `Bearer ${token}`)
       .send({ name: 'Projeto Teste' });
     expect(res.status).toBe(201);
@@ -74,52 +77,89 @@ describe('Kanban API - Testes de Integração', () => {
 
   it('Deve adicionar usuários ao projeto', async () => {
     await request(app.getHttpServer())
-      .post(`/api/v1/projects/${projectId}/members`)
-      .set('Authorization', `Bearer ${token}`)
-      .send({ userId: userId1 })
-      .expect(201);
-
-    await request(app.getHttpServer())
-      .post(`/api/v1/projects/${projectId}/members`)
+      .post(`/projects/${projectId}/members`)
       .set('Authorization', `Bearer ${token}`)
       .send({ userId: userId2 })
       .expect(201);
+
+    await request(app.getHttpServer())
+      .post(`/projects/${projectId}/members`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: userId2 })
+      .expect(403);
   });
 
   it('Deve criar uma tarefa', async () => {
     const res = await request(app.getHttpServer())
-      .post('/api/v1/tasks')
-      .set('Authorization', `Bearer ${token}`)
+      .post('/tasks')
+      .set('Authorization', `Bearer ${token2}`)
       .send({ title: 'Tarefa Teste', projectId });
+
     expect(res.status).toBe(201);
     taskId = res.body.id;
   });
 
-  it('Deve atribuir usuários à tarefa', async () => {
-    await request(app.getHttpServer())
-      .post(`/api/v1/tasks/${taskId}/assign`)
-      .set('Authorization', `Bearer ${token}`)
-      .send({ userId: userId1 })
-      .expect(201);
+  it('should list members of a project', async () => {
+    const res = await request(app.getHttpServer())
+      .get(`/projects/${projectId}/members`)
+      .set('Authorization', `Bearer ${token}`);
 
-    await request(app.getHttpServer())
-      .post(`/api/v1/tasks/${taskId}/assign`)
+    console.log(res.body);
+
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
+  });
+
+  it('should list tasks of a project', async () => {
+    const res = await request(app.getHttpServer())
+      .get(`/projects/${projectId}/tasks`)
+      .set('Authorization', `Bearer ${token2}`);
+
+    console.log('list of tasks', res.body);
+
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
+  });
+
+  it('should list members of a task', async () => {
+    const res = await request(app.getHttpServer())
+      .get(`/tasks/${taskId}/users`)
+      .set('Authorization', `Bearer ${token}`);
+
+    console.log(res.body);
+
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
+  });
+
+  it('Deve atribuir usuários à tarefa', async () => {
+    const res = await request(app.getHttpServer())
+      .post(`/tasks/${taskId}/assign`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: userId1 });
+
+    console.log(res.body);
+
+    const res2 = await request(app.getHttpServer())
+      .patch(`/tasks/${taskId}/assign`)
       .set('Authorization', `Bearer ${token}`)
       .send({ userId: userId2 })
       .expect(201);
+
+    console.log(res2);
   });
 
   it('Deve remover usuários da tarefa', async () => {
     await request(app.getHttpServer())
-      .delete(`/api/v1/tasks/${taskId}/assign`)
+      .patch(`/tasks/${taskId}/unassign`)
       .set('Authorization', `Bearer ${token}`)
-      .send({ userId: userId1 })
+      .send({ userId: userId2 })
       .expect(200);
   });
 
   it('Deve remover usuários do projeto', async () => {
     await request(app.getHttpServer())
-      .delete(`/api/v1/projects/${projectId}/members`)
+      .delete(`/projects/${projectId}/members`)
       .set('Authorization', `Bearer ${token}`)
       .send({ userId: userId2 })
       .expect(200);
